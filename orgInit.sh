7bc633577f0eaa5f8e7461d527d76d8ebddea98e
Stefan Zepeda <omarat@me.com>
2024-10-21 15:19:44 -0400

    Update orgInit.sh to new commands

--- orgInit.sh
+++ orgInit.sh
@@ -1,22 +1,23 @@
 # Create the demo org
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix transaction -o security.demo
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p transaction -e security.demo -w 60
+
 #sfdx force:org:create -f config/project-scratch-def.json -a sfdc-security-demo
 
 # Updates the Transaction Security Policies to contain this scratch orgs username
 sfdx shane:tsp:username:update
 
 # Push the metadata into the new scratch org.
-sfdx force:source:push
+sf project deploy start
 
 # Assign user the permset
-sfdx force:user:permset:assign -n TransactionSecurity
+sf org assign permset -n TransactionSecurity
 
 # Set the default password.
 sf demoutil user password set -p salesforce1 -g User -l User
 
 
 # Open the org.
-sfdx force:org:open
+sf org open
 
 # Import the data required by the demo
 sfdx automig:load --inputdir ./data
