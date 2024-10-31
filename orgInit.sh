d6f6b97bee05f4a16be74df9bcb118d00c68efa0
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:08:03 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,25 +1,8 @@
-# Create the demo org
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p transaction -e security.demo -w 60
-
-#sfdx force:org:create -f config/project-scratch-def.json -a sfdc-security-demo
-
-# Updates the Transaction Security Policies to contain this scratch orgs username
 sfdx shane:tsp:username:update
-
-# Push the metadata into the new scratch org.
 sf project deploy start
-
-# Assign user the permset
 sf org assign permset -n TransactionSecurity
-
-# Set the default password.
 sf demoutil user password set -p salesforce1 -g User -l User
-
-
-# Open the org.
+sf automig load --inputdir ./data
+sf apex run -f ./script/apex/createFile.apex
 sf org open
-
-# Import the data required by the demo
-sfdx automig:load --inputdir ./data
-
-sfdx force:apex:execute -f ./script/apex/createFile.apex
